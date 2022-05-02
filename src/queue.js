6139ec33da87d9670490f40945cf55b934545306
Max Kovalenko <tutmrmaxi@gmail.com>
2022-05-02 18:43:54 +0300

    feat: add some solutions

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor () {
+    this.first = 0;
+    this.last = 0;
+    this.length = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    let element = new ListNode (value);
+    if(this.length == 0) {
+      this.first = element; 
+      this.last = element;
+      ++this.length;
+    }else {
+      this.last.next = element;
+      this.last = element;
+      ++this.length;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let deleted = this.first;
+    this.first = this.first.next;
+    --this.length;
+    return deleted.value;
+  }
+
+  getUnderlyingList() {
+    return this.first;
   }
 }
 
